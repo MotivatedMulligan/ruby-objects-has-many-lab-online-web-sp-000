3f1f579248a7574dcdd7339ef10c2053ec1ae666
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-03-22 18:27:00 +0000

    Automatically backed up by Learn

---
+++ lib/post.rb
@@ -0,0 +1,25 @@
+class Post
+  attr_accessor :title, :author
+  @@all = [] #works
+  def initialize(title) #works
+    @title = title
+    @@all << self
+  end
+  def self.all #works
+    @@all
+  end
+  def title=(title) #works
+  @title = title
+  end
+  def author=(author) #works
+  @author = author
+  end
+###################################
+  def author_name(author)
+    if (self.author.nil?)
+      self.author = Author.new(name)
+    else
+      self.author.name = name
+    end
+  end
+end
---
+++ lib/song.rb
@@ -0,0 +1,26 @@
+class Song
+  attr_accessor :artist, :name
+    @@all = [] #works
+  def initialize(name) #works
+    @name = name
+    #@songs = song
+    @@all << self
+  end
+  def self.all #works
+    @@all
+  end
+  def name=(name) #works
+    @name = name
+  end
+  def artist=(artist) #works
+    @artist = artist
+  end
+  ##################################
+  def artist_name(name)
+    if (self.artist.nil?)
+      self.artist = Artist.new(name)
+    else
+      self.artist.name = name
+    end
+  end
+end
